6d366fa40639a2dea914fcecf940a4455b386794
lxcsige <lxcshang@163.com>
2022-03-02 00:20:27 +0800

    fix

--- src/com/dp/algorithm/dp/stock/MaxProfit.java
+++ src/com/dp/algorithm/dp/stock/MaxProfit.java
@@ -56,4 +56,23 @@ public class MaxProfit {
         }
         return res;
     }
+
+    /**
+     * 股票问题通用解法，将无后效性的约束条件转化为状态参数
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit4(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][1] + prices[i], dp[i-1][0]);
+            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+        }
+
+        return dp[prices.length-1][0];
+    }
 }
